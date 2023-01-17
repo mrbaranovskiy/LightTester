5fe87b1e2e39c678fa482f8949efddc80b750854
Dmytro Baranovskyi <dmytro.o.baranovskyi@globallogic.com>
2023-01-17 23:27:31 +0200

    Update

--- LightService/Worker.cs
+++ LightService/Worker.cs
@@ -31,7 +31,7 @@ public class Worker : BackgroundService
     {
         _logger.LogInformation("Start service");
         _botClient = new TelegramBotClient(token);
-        _networkChecker = new NetworkChecker();
+        _networkChecker = new NetworkChecker(_logger);
         
         _eventSource = Observable.FromEventPattern<EventHandler<LightState>, LightState>(
             hv => _networkChecker.OnStateChanged += hv,
@@ -74,11 +74,11 @@ public class Worker : BackgroundService
         return base.StopAsync(cancellationToken);
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         ReceiverOptions receiverOptions = new()
         {
-            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+            AllowedUpdates = Array.Empty<UpdateType>()
         };
 
         _logger.LogInformation("Start client initializing");
@@ -87,12 +87,41 @@ public class Worker : BackgroundService
             pollingErrorHandler: HandlePollingErrorAsync,
             receiverOptions: receiverOptions,
             cancellationToken: _cts.Token);
+
+        return Task.CompletedTask;
+    }
+    
+    private string GetStatistics()
+    {
+        return ReadFromDb().Aggregate((n, m) => n + Environment.NewLine + m);
+    }
+
+    private List<string> ReadFromDb()
+    {
+        try
+        {
+            using var fs = System.IO.File.OpenText("");
+            var buffer = new List<string>();
+
+            while (fs.ReadLine() is { } str)
+                buffer.Add(str);
+
+            return buffer.Count < 10 ? buffer : buffer.TakeLast(10).ToList();
+        }
+        catch (Exception e)
+        {
+            return new List<string>();
+        }
     }
 
     private static void RecordToDb(LightState state)
     {
-        
+        using var fs = System.IO.File.OpenWrite("");
+        using var sw = new StreamWriter(fs);
+        sw.WriteLine($"State {state.state} on {state.time:s}");
     }
+  
+    
 
     private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
         CancellationToken cancellationToken)
@@ -175,7 +204,7 @@ public class Worker : BackgroundService
 
     private string ResponseCheckLight(States state, DateTime time) => state switch
     {
-        States.On => $"😊 since {time:t}",
-        States.Off => $"🕶 since {time:t}"
+        States.On => $"😊 since {time:f}",
+        States.Off => $"🕶 since {time:f}"
     };
 }
